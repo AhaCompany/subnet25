72eb5f7788e8ca606f9957a4aac476d2b73ad9e5
Hưng Trần mạnh <hungtranmanh@Hungs-MacBook-Pro-2.local>
2025-03-14 16:07:20 +0700

    doing

--- pm2_configs/miner.config.js
+++ pm2_configs/miner.config.js
@@ -3,6 +3,10 @@ module.exports = {
         name: "folding-miner-c5-h1",
         script: "scripts/run_miner.sh", // Use the wrapper script
         autorestart: true,
-        watch: false
+        watch: false,
+        env: {
+            SKIP_RQLITE: "1",
+            RQLITE_HTTP_ADDR: "localhost:4001"
+        }
     }]
 };
